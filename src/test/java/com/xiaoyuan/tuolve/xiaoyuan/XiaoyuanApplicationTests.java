403de89c9528006996be8a1920abde967ed3831d
seerller <lb13936412292@live.com>
2019-11-25 15:29:31 +0800

    xiaoyuan springboot

---
+++ src/test/java/com/xiaoyuan/tuolve/xiaoyuan/XiaoyuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyuan.tuolve.xiaoyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoyuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ xiaoyuan-webapi/src/main/java/com/xiaoyuan/XiaoyuanSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.xiaoyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class XiaoyuanSpringbootApplication extends SpringBootServletInitializer{
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoyuanSpringbootApplication.class, args);
+    }
+}
