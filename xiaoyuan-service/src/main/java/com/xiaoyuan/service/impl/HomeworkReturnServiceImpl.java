921ce11f4cec51c5c917ca34e98eead5ef46d09f
seerller <lb13936412292@live.com>
2019-11-26 11:41:11 +0800

    mapper debugs

--- xiaoyuan-service/src/main/java/com/xiaoyuan/service/impl/HomeworkReturnServiceImpl.java
+++ xiaoyuan-service/src/main/java/com/xiaoyuan/service/impl/HomeworkReturnServiceImpl.java
@@ -1,7 +1,7 @@
 package com.xiaoyuan.service.impl;
 
 import com.xiaoyuan.model.HomeworkReturn;
-import com.xiaoyuan.mapper.HomeworkReturnMapper;
+import com.xiaoyuan.mapper.Homework_returnMapper;
 import com.xiaoyuan.service.IHomeworkReturnService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
  * @since 2019-11-26
  */
 @Service
-public class HomeworkReturnServiceImpl extends ServiceImpl<HomeworkReturnMapper, HomeworkReturn> implements IHomeworkReturnService {
+public class HomeworkReturnServiceImpl extends ServiceImpl<Homework_returnMapper, HomeworkReturn> implements IHomeworkReturnService {
+
 
 }
