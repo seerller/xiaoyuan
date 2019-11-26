23ae1a1c1bc990f3f86c578773cf92a4eaff47a1
seerller <lb13936412292@live.com>
2019-11-26 15:48:50 +0800

    controller

--- xiaoyuan-webapi/src/main/java/com/xiaoyuan/controller/SchoolController.java
+++ xiaoyuan-webapi/src/main/java/com/xiaoyuan/controller/SchoolController.java
@@ -1,9 +1,17 @@
 package com.xiaoyuan.controller;
 
 
+import com.github.pagehelper.ISelect;
+import com.xiaoyuan.service.impl.SchoolServiceImpl;
+import com.xiaoyuan.tools.MessageBean;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +24,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/mapper/school")
 public class SchoolController {
+    @Autowired
+    SchoolServiceImpl schoolService;
+    @Test
+    @RequestMapping(value = "/List<School>" , method = RequestMethod.GET)
+    public MessageBean selectByPrimaryKey(){
+        return selectByPrimaryKey();
+    }
+
 
 }
