06d3a3bc1d4061a75d76f4f6096648ce65856d60
seerller <lb13936412292@live.com>
2019-11-29 15:21:51 +0800

    order module

--- xiaoyuan-service/src/main/java/com/xiaoyuan/service/impl/OrderServiceImpl.java
+++ xiaoyuan-service/src/main/java/com/xiaoyuan/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.xiaoyuan.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.additional.query.impl.LambdaQueryChainWrapper;
 import com.baomidou.mybatisplus.extension.service.additional.query.impl.QueryChainWrapper;
@@ -12,6 +13,7 @@ import com.xiaoyuan.service.IOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +28,7 @@ import java.util.function.Function;
  * @since 2019-11-26
  */
 @Service
-public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements IOrderService {
+public class OrderServiceImpl implements IOrderService {
 
     @Override
     public int deleteByPrimaryKey(Integer orderId) {
@@ -59,12 +61,57 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     }
 
     @Override
+    public boolean save(Order entity) {
+        return false;
+    }
+
+    @Override
     public boolean saveBatch(Collection<Order> entityList) {
         return false;
     }
 
     @Override
+    public boolean saveBatch(Collection<Order> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
     public boolean saveOrUpdateBatch(Collection<Order> entityList) {
+        return false;
+    }
+
+    @Override
+    public boolean saveOrUpdateBatch(Collection<Order> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
+    public boolean removeById(Serializable id) {
+        return false;
+    }
+
+    @Override
+    public boolean removeByMap(Map<String, Object> columnMap) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Wrapper<Order> queryWrapper) {
+        return false;
+    }
+
+    @Override
+    public boolean removeByIds(Collection<? extends Serializable> idList) {
+        return false;
+    }
+
+    @Override
+    public boolean updateById(Order entity) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Order entity, Wrapper<Order> updateWrapper) {
         return false;
     }
 
@@ -79,8 +126,53 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     }
 
     @Override
+    public boolean updateBatchById(Collection<Order> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
+    public boolean saveOrUpdate(Order entity) {
+        return false;
+    }
+
+    @Override
+    public Order getById(Serializable id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Order> listByIds(Collection<? extends Serializable> idList) {
+        return null;
+    }
+
+    @Override
+    public Collection<Order> listByMap(Map<String, Object> columnMap) {
+        return null;
+    }
+
+    @Override
     public Order getOne(Wrapper<Order> queryWrapper) {
         return null;
+    }
+
+    @Override
+    public Order getOne(Wrapper<Order> queryWrapper, boolean throwEx) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getMap(Wrapper<Order> queryWrapper) {
+        return null;
+    }
+
+    @Override
+    public <V> V getObj(Wrapper<Order> queryWrapper, Function<? super Object, V> mapper) {
+        return null;
+    }
+
+    @Override
+    public int count(Wrapper<Order> queryWrapper) {
+        return 0;
     }
 
     @Override
@@ -89,12 +181,27 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     }
 
     @Override
+    public List<Order> list(Wrapper<Order> queryWrapper) {
+        return null;
+    }
+
+    @Override
     public List<Order> list() {
         return null;
     }
 
     @Override
+    public IPage<Order> page(IPage<Order> page, Wrapper<Order> queryWrapper) {
+        return null;
+    }
+
+    @Override
     public IPage<Order> page(IPage<Order> page) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> listMaps(Wrapper<Order> queryWrapper) {
         return null;
     }
 
@@ -119,7 +226,22 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     }
 
     @Override
+    public <V> List<V> listObjs(Wrapper<Order> queryWrapper, Function<? super Object, V> mapper) {
+        return null;
+    }
+
+    @Override
+    public IPage<Map<String, Object>> pageMaps(IPage<Order> page, Wrapper<Order> queryWrapper) {
+        return null;
+    }
+
+    @Override
     public IPage<Map<String, Object>> pageMaps(IPage<Order> page) {
+        return null;
+    }
+
+    @Override
+    public BaseMapper<Order> getBaseMapper() {
         return null;
     }
 
