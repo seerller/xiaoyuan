37ee1053c3c509f22c424963ab69d29cbdda6a25
seerller <lb13936412292@live.com>
2019-12-02 17:14:20 +0800

    Announcement module config

--- xiaoyuan-webapi/src/main/java/com/xiaoyuan/controller/AnnouncementController.java
+++ xiaoyuan-webapi/src/main/java/com/xiaoyuan/controller/AnnouncementController.java
@@ -1,20 +1,39 @@
 package com.xiaoyuan.controller;
 
 
+import com.xiaoyuan.service.impl.AnnouncementServiceImpl;
+import com.xiaoyuan.tools.MessageBean;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ *  公告控制器
  * </p>
  *
  * @author jobob
  * @since 2019-11-26
  */
+@Api(value = "/Announcement", description = "公告管理模块")
 @RestController
 @RequestMapping("/mapper/announcement")
 public class AnnouncementController {
+    @Autowired
+    AnnouncementServiceImpl announcementService;
+    @RequestMapping(value = "/selectAll", method = RequestMethod.GET)
+    @ApiOperation(value = "查询公告", notes = "公告信息")
+    public MessageBean selectAll(){
+        return selectAll();
+    }
+    @RequestMapping(value = "/deleteByPrimaryKey", method = RequestMethod.POST)
+    @ApiOperation(value = "删除公告", notes = "删除公告")
+    public void deleteByPrimaryKey(Integer announcementId){
+        announcementService.deleteByPrimaryKey(announcementId);
+    }
 
 }
